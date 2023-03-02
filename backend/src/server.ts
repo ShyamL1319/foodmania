38064fd642f8a1aee2ad0a061e60bc9349e35155
ShyamL1319 <sshyamlalchauhan@gmail.com>
2023-03-03 04:29:26 +0530

    prod ready foodmaia

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.route"
 import orderRoutes from "./routes/order.route"
 import { dbConnect } from './configs/db.config';
 import authMid from './middlewares/auth.mid';
+import path from "path";
 dbConnect();
 const app = express();
 app.use(express.json());
@@ -17,7 +18,10 @@ app.use(cors({
 app.use("/api/foods", foodRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders",[authMid], orderRoutes);
-
+app.use(express.static("public"));
+app.get("*", (req, res) => { 
+    res.sendFile(path.join(__dirname,'public','index.html'));
+})
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => { 
